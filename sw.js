734ad3601231718ab54986f6c1e3ade08c7ed39e
Koe <duttaanish458@gmail.com>
2026-07-18 07:37:34 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@
    ═══════════════════════════════════════════════════════════════════ */
 'use strict';
 
-const CACHE_VERSION = 'v8';
+const CACHE_VERSION = 'v9';
 const SHELL = `dz-shell-${CACHE_VERSION}`;
 const THUMB = `dz-thumb-${CACHE_VERSION}`;
 const VIEW  = `dz-view-${CACHE_VERSION}`;
